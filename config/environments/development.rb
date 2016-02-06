c0451048df88bb592f097a362da57d09229f7700
saqib sohail <saqibsohail007@gmail.com>
2016-02-06 15:45:27 +0500

    all done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   Paperclip.options[:command_path] = 'C:\Program Files (x86)\GnuWin32\bin'
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.cache_classes = false
 
   # Do not eager load code on boot.
